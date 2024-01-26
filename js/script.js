f15662e18e9cf751da0508bf7ac138057214c632
albertoferri <falberto99@gmail.com>
2024-01-26 16:11:36 +0100

    fix inserimento età in caratteri

--- js/script.js
+++ js/script.js
@@ -2,23 +2,35 @@
 const range = prompt("Ciao!!, qual'è il numero di km che vuoi percorrere?");
 const age = prompt("Quanti anni hai?");
 
-// sconto young
-let price = range * 0.21;
+// verifica input
+if (isNaN(age)){
+    alert("Inserisci una data valida!")
+} else if (range = "-"){
+    alert("Inserisci un numero valido!")
+}
 
+
+
+// tariffa standard
+let price = range * 0.21;
+let prezzoStandard = price.toFixed(2);
+
+// sconto young
 let scontoYoung = price - ((price * 20) / 100);
 let prezzoYoung2ch = scontoYoung.toFixed(2);
 
 // sconto senior
 let scontoSenior = price - ((price * 40) / 100);
 let prezzoSenior2ch = scontoSenior.toFixed(2);
+
 if (age < 18){
     document.getElementById("yourWay").innerHTML = "Dato che rientri nella fascia 'Young' il prezzo della tua tratta è di: " + prezzoYoung2ch + "€";
 } else if (age > 64){
     document.getElementById("yourWay").innerHTML = "Dato che rientri nella fascia 'Senior' il prezzo della tua tratta è di: " + prezzoSenior2ch + "€";
 } else{
-    document.getElementById("yourWay").innerHTML = "Il prezzo della tua tratta è di: " + price + "€";
+    document.getElementById("yourWay").innerHTML = "Il prezzo della tua tratta è di: " + prezzoStandard + "€";
 }
 
 
 // console log
-console.log("Questo è il prezzo della tratta standard: " + price, "Prezzo young scontato: " + prezzoYoung2ch, "Prezzo young scontato: " + prezzoSenior2ch)
+console.log("Questo è il prezzo della tratta standard: " + price, "Prezzo young scontato: " + prezzoYoung2ch, "Prezzo young scontato: " + prezzoSenior2ch);
