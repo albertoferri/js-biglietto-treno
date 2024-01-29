b8e9c78fad7290e9ebd757909ae0acd1f3229266
albertoferri <falberto99@gmail.com>
2024-01-29 13:04:22 +0100

    controllo se il numero è valido

--- snack-temp/js/script.js
+++ snack-temp/js/script.js
@@ -4,3 +4,17 @@ let celsiusInput = prompt("Inserisci la temperatura in gradi Celsius:");
 // Converti l'input in un numero
 let celsius = parseFloat(celsiusInput);
 
+// input per convertire da Celsius a Fahrenheit
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+// Controlla se l'input è un numero valido
+if (isNaN(celsius)) {
+    alert("Inserisci un numero valido per la temperatura in gradi Celsius.");
+} else {
+    let fahrenheit = celsiusToFahrenheit(celsius);
+
+// Mostra il risultato
+document.getElementById("temp").innerHTML =  (celsius + " °C sono equivalenti a " + fahrenheit.toFixed(2) + " °F.");
+}
