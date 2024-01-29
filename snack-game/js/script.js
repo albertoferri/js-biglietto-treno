3d285eb738180e8f4fd3ec9300a0aea8885833e3
albertoferri <falberto99@gmail.com>
2024-01-29 18:16:36 +0100

    FIX SNACK 3

--- snack-game/js/script.js
+++ snack-game/js/script.js
@@ -3,6 +3,23 @@ let userChoice = prompt("Cosa giochi?")
 
 // imposto variabili per sasso carta e forbice
 const arrayScelte = ['sasso', 'carta', 'forbice']
-const randomChoice = arrayScelte[Math.floor(Math.random() * arrayScelte.lenght)];
+let numeroRandom = Math.floor(Math.random() * arrayScelte.length)
+const randomChoice = arrayScelte[Math.floor(Math.random() * arrayScelte.length)];
 
-console.log(randomChoice)
+// input
+if (userChoice === "sasso" && randomChoice === "carta"){
+    document.getElementById("result").innerHTML = "HAI PERSO! AHAHAHA";
+} else if (userChoice === "carta" && randomChoice === "forbice"){
+    document.getElementById("result").innerHTML = "HAI PERSO! AHAHAHA";
+} else if (userChoice === "forbice" && randomChoice === "sasso"){
+    document.getElementById("result").innerHTML = "HAI PERSO! AHAHAHA";
+} else if (userChoice === randomChoice){
+    document.getElementById("result").innerHTML = "PARITA'"
+} else {
+    document.getElementById("result").innerHTML = "HAI VINTO!";
+}
+
+// console log
+console.dir(arrayScelte);
+console.log("Questa è la tua scelta: " + userChoice)
+console.log("Questa è la scelta del computer: " + randomChoice)
